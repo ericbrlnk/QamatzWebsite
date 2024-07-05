eb1ebd7df4e4b9b4f90d4305f3b6c6e03f23d25d
Lukasz Molinek <hilbertmol@googlemail.com>
2024-07-06 01:26:03 +0200

    added db-context class

---
+++ QamatzWebsite/Data/QamatzDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.Extensions.Options;
+using QamatzWebsite.Models;
+
+namespace QamatzWebsite.Data
+{
+    public class QamatzDBContext : DbContext
+    {
+        public QamatzDBContext(DbContextOptions<QamatzDBContext> options) : base(options)
+        {
+        }
+
+        // Database results
+        // Post
+        public DbSet<Post> Post { get; set; }
+        
+        // Tag
+        public DbSet<Tag> Tag { get; set; }
+        
+        // PostTag
+        public DbSet<PostTag> PostTag { get; set; }
+
+    }
+}
--- QamatzWebsite/Program.cs
+++ QamatzWebsite/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using QamatzWebsite.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<QamatzDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnQamatz")));
+
+// Add services to the container
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
