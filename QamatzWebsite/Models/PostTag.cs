b6f0f0fdbe2462b9b0fd998b5fcf004f24c0d7d2
Lukasz Molinek <hilbertmol@googlemail.com>
2024-07-06 01:34:53 +0200

    added first db migration

--- QamatzWebsite/Models/PostTag.cs
+++ QamatzWebsite/Models/PostTag.cs
@@ -1,8 +1,10 @@
-﻿namespace QamatzWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QamatzWebsite.Models
 {
     public class PostTag
     {
-
+        [Key]
         public int PostId { get; set; }
 
         public int TagId { get; set; }
