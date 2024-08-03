176807e1e7bc0353f4438c853fd102759bc68c69
Lukasz Molinek <hilbertmol@googlemail.com>
2024-08-04 00:36:21 +0200

    updated program class (authentication with jwt)

--- QamatzWebsite/Program.cs
+++ QamatzWebsite/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Authentication;
 using QamatzWebsite.Data;
 using QamatzWebsite.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +18,61 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(item =>
+{
+    item.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "QamatzUserAPI", Version = "v1" });
+    item.AddSecurityDefinition("Bearer scheme", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization using Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    item.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddDbContext<UserDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnUser")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<UserDBContext>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(item =>
+{
+    var secret = builder.Configuration.GetValue<string>("UserSecret");
+    var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+
+    item.RequireHttpsMetadata = true;
+    item.SaveToken = true;
+    item.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidAudience = "https://localhost:6000",
+        ValidIssuer = "https://localhost:6000",
+        IssuerSigningKey = key,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
 
 builder.Services.AddDbContext<PostDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnPost")));
 
@@ -23,6 +80,7 @@ var app = builder.Build();
 
 // allow communication from port 3000
 app.UseCors(options => options.WithOrigins("https://localhost:3000")
+.AllowAnyOrigin()
 .AllowAnyMethod()
 .AllowAnyHeader()
 );
@@ -36,6 +94,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
